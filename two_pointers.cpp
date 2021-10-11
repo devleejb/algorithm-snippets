fc7701b1133a6824e6e6a6d293a9842a197919d0
leejongbeom <dev.leejb@gmail.com>
2021-10-11 15:01:04 +0900

    Two Pointers

---
+++ two_pointers.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+using namespace std;
+
+int intervalSum, cnt, targetSum = 5;
+int arr[5] = {1, 2, 3, 4, 5};
+
+
+void calculateIntervalSum(int N) {
+    int end = 0;
+
+    for (int start = 0; start < N; ++start) {
+        while (intervalSum < targetSum && end < N) {
+            intervalSum += arr[end];
+            ++end;
+        }
+
+        if (intervalSum == targetSum) ++cnt;
+
+        intervalSum -= arr[start];
+    }
+}
+
+int main() {
+    calculateIntervalSum(5);
+
+    printf("%d", cnt);
+
+    return 0;
+}
